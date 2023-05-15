f3b627401ad9e9e088cb3f841191b7c82b9dcece
unknown <ricknogueira1231@gmail.com>
2023-05-15 18:28:11 -0300

    Criando Controller User

---
+++ src/main/java/br/hkprogrammer/fullstack/controllers/UsersControllers.java
@@ -0,0 +1,55 @@
+package br.hkprogrammer.fullstack.controllers;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.hkprogrammer.fullstack.entities.Users;
+import br.hkprogrammer.fullstack.service.UsersServices;
+
+public class UsersControllers {
+
+	@Autowired
+	private UsersServices service;
+	
+	@GetMapping
+	public List<Users> findAll() {
+		List<Users> list = service.findAll();
+		return list;
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> findById(@PathVariable Long id) {
+		ResponseEntity<?> obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Users> create(@RequestBody Users obj) {
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Users> update(@RequestBody Users obj, @PathVariable Long id) {
+		obj = service.update(obj, id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+}
