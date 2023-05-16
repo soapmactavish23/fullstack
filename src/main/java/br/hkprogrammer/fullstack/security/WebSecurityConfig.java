2c653de3ac24abed36553e5a36d8e9835d365b73
unknown <ricknogueira1231@gmail.com>
2023-05-16 16:48:34 -0300

    Criando e testando arquivo de configuração WebSecurityConfig

---
+++ src/main/java/br/hkprogrammer/fullstack/security/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package br.hkprogrammer.fullstack.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeHttpRequests((authz) -> {
+			try {
+				authz.requestMatchers(HttpMethod.POST, "/users").permitAll().anyRequest().authenticated().and().cors();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+		return http.build();
+	}
+
+}
